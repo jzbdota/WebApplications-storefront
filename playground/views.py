4470583798f00ccfeb2d7b83e7a91866886f4fd5
Yi Yang <yiy042@ucsd.edu>
2023-09-28 16:36:00 -0700

    F object to convert string to a field

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import F
 from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F('unit_price'))
     context = {
         'name' : 'Yi',
         'products': list(queryset)
