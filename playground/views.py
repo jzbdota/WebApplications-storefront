08dcfb642e417590199856ca5595a6b32237f327
Yi Yang <yiy042@ucsd.edu>
2023-09-27 22:08:14 -0700

    Add template in playground

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello world!")
+    context = {
+        'name' : 'Yi'
+    }
+    return render(request, 'hello.html', context)
