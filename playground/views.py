c0c6749579f9ab2570b0352e4710dfe7c6a71c85
Yi Yang <yiy042@ucsd.edu>
2023-09-28 16:30:38 -0700

    Q object to do 'or' operation in filter

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q
+from store.models import Product
 
 # Create your views here.
 def say_hello(request):
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     context = {
-        'name' : 'Yi'
+        'name' : 'Yi',
+        'products': list(queryset)
     }
     return render(request, 'hello.html', context)
