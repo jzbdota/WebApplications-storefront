762e67bbae3880d592646906dd8a695bd2a545d4
Yi Yang <yiy042@ucsd.edu>
2023-09-27 23:03:30 -0700

    Added 2 apps store and tags In the tags app, I used content type for generic. In the store app, multiple models were added.

---
+++ store/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+
+# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    # circular dependency
+    # do not create the reverse relationship by related_name = "+"
+    featured_product = models.ForeignKey(
+        'Product',
+        on_delete = models.SET_NULL,
+        null = True,
+        related_name="+"
+    )
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    unit_price = models.DecimalField(6, 2)
+    inventory = models.PositiveIntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    # do not delete product if we delete the collection
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null = True)
+    membership = models.CharField(
+        max_length = 1,
+        choices = MEMBERSHIP_CHOICES,
+        default = 'B'
+        )
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1,
+        choices = PAYMENT_STATUS_CHOICES,
+        default='P'
+    )
+    # never delete orders, even the customer is deleted
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(6, 2)
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        )
+
