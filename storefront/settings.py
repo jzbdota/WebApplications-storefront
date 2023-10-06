a5f1a58ef89482059896310736857700aa445dfe
Yi Yang <yiy042@ucsd.edu>
2023-10-05 19:13:53 -0700

    Installed Djoser and jwt.

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework',
     'debug_toolbar',
+    'djoser',
     'playground',
     'store',
     'tags',
@@ -141,7 +142,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 # not sure why not 'core.models.User'
 AUTH_USER_MODEL = 'core.User'
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
