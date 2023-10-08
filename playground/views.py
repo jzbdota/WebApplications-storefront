e6ae5b83adf079b3ff72680875f519aae1f60b85
Yi Yang <yiy042@ucsd.edu>
2023-10-07 19:46:09 -0700

    Added smtp for emails.

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,20 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 from django.shortcuts import render
-from django.db.models import F
-from store.models import Product, OrderItem, Order
+from templated_mail.mail import BaseEmailMessage
 
 # Create your views here.
 def say_hello(request):
-    queryset = Product.objects.filter(inventory=F('unit_price'))
-
-    context = {
-        'name' : 'Yi',
-        'products': list(queryset)
-    }
-    return render(request, 'hello.html', context)
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Yi'}
+        )
+        message.send(['john@yiyang.com'])
+        # message = EmailMessage('subject', 'message', 'from@yiyang.com', ['john@yiyang.com'])
+        # message.attach_file('playground/static/images/photo.jpg')
+        # message.send()
+        # mail_admins('subject', 'message', html_message='message')
+        # send_mail('subject', 'message', 'info@yiyang.com', ['bob@yiyang.com'])
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html')
--- storefront/settings.py
+++ storefront/settings.py
@@ -175,3 +175,14 @@ DJOSER = {
         'current_user': 'core.serializers.UserSerializer',
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # not necessary
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525 # fake, usually 25
+DEFAULT_FROM_EMAIL = 'from@yiyang.com'
+
+ADMINS = [
+    ('Yi', 'admin@yiyang.com')
+]
