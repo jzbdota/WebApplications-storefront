a5f802a20f79cdc81b5ef2dfc7158f54d053d123
YiYang/WSL <yiy042@ucsd.edu>
2023-10-12 21:49:23 -0700

    Added Low-level Cach API.

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -24,6 +24,10 @@ class WebSiteUser(HttpUser):
                          name='/store/carts/items',
                          json={'product_id': product_id,
                                'quantity': 1})
+    
+    @task(10)
+    def say_hello(self):
+        self.client.get('/playground/hello/')
 
     def on_start(self):
         response = self.client.post('/store/carts/')
--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,12 @@
+import requests
 from django.shortcuts import render
-from .tasks import notify_customers
+from django.core.cache import cache
 
 # Create your views here.
 def say_hello(request):
-    notify_customers.delay('hello')
-    return render(request, 'hello.html')
+    key = 'httpbin_result'
+    if cache.get(key) is None:
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        cache.set(key, data)
+    return render(request, 'hello.html', {'name': cache.get(key)})
